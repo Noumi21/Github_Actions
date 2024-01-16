c065c6d3928a5fe7ce50bc388cc40cc2d85ed81b
Noumi21 <149524278+Noumi21@users.noreply.github.com>
2024-01-16 23:02:43 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import pytest
-import monitor
+import libreria_bot
 
 TOKEN_TELEGRAM_BOT = '6960080306:AAEQqlt1I-3g1I_r3d5kCWBjS3qAS59cgbg'
 id_chat = '6527212555'
@@ -7,13 +7,13 @@ mensaje = "Se ha ejecutado el test_main correctamente"
 
 def test_check_port_status():
     # Suponiendo que esta función devuelve True/False
-    assert monitor.check_port_status("http://scanme.nmap.org/") is not None
+    assert libreria_bot.check_port_status("http://scanme.nmap.org/") is not None
 
 def test_check_service_response():
     # Suponiendo que esta función devuelve True/False
-    assert monitor.check_service_response("http://scanme.nmap.org/") is not None
+    assert libreria_bot.check_service_response("http://scanme.nmap.org/") is not None
 
 def enviar_mensaje():
-    monitor.enviar_mensaje_telegram(TOKEN_TELEGRAM_BOT, id_chat, mensaje)
+    libreria_bot.enviar_mensaje_telegram(TOKEN_TELEGRAM_BOT, id_chat, mensaje)
 
 enviar_mensaje() # Llama a esta función para enviar el mensaje.
